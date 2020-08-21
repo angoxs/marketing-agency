bb53350ae09963fc52078409dc9d9c61f416517f
Angel <angelCL0@outlook.com>
2020-08-21 14:39:45 -0400

    Better

--- components/Works/Works.js
+++ components/Works/Works.js
@@ -8,16 +8,16 @@ export default function Works() {
       <Main>
         <Title>Amazing Works</Title>
         <WorksGrid>
-          <Fade left>
+          <Fade up>
             <Work image="https://images.unsplash.com/photo-1453133451515-5ff7c1d0d63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80" />
           </Fade>
-          <Fade right>
+          <Fade left>
             <Work image="https://images.unsplash.com/photo-1519211975560-4ca611f5a72a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" />
           </Fade>
           <Fade left>
             <Work image="https://images.unsplash.com/photo-1519222970733-f546218fa6d7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80s" />
           </Fade>
-          <Fade right>
+          <Fade up>
             <Work image="https://images.unsplash.com/photo-1519127008657-d552258f18d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80" />
           </Fade>
         </WorksGrid>
