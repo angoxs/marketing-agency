2960d498a013a5dc002e86915456633ecdabf07a
Angel <angelCL0@outlook.com>
2020-08-23 16:04:50 -0400

    Awesome

--- components/FooterClip/style.js
+++ components/FooterClip/style.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 20px;
+  padding: 20px 30px;
   background: #1b1b86;
   position: relative;
   bottom: 0;
   right: 0;
   left: 0;
+
+  @media (max-width: 900px) {
+    padding: 20px;
+  }
 `;
 
 export const Clip = styled.div`
