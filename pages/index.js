b721d23e968f1d66f3590a37c26dc6df3c352947
Angel <angelCL0@outlook.com>
2020-08-23 16:14:59 -0400

    better

--- pages/index.js
+++ pages/index.js
@@ -35,8 +35,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Vercel 🖤
         </a>
       </footer>
     </>
