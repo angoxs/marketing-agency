b0d03957cb2cf653272ead6b13c1e17a43854ba9
Angel <angelCL0@outlook.com>
2020-08-23 21:39:39 -0400

    Work

--- components/FooterClip/FooterClip.js
+++ components/FooterClip/FooterClip.js
@@ -5,9 +5,10 @@ export default function FooterClip() {
   return (
     <Container>
       <Clip>
-        <a href="https://twitter.com/angelVZUR">
+        {/* <a href="https://twitter.com/angelVZUR">
           Copyright © 2020 Ángel Vāzquez, Inc. All rights reserved.
-        </a>
+        </a> */}
+        <a href="https://twitter.com/angelVZUR">© 2020 Ángel Vāzquez</a>
         <a href="mailto:angelCL@outlook.com">angelCL0@outlook.com</a>
       </Clip>
     </Container>
